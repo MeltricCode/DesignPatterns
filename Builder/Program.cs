a4c2261f4566b9491832db2ac3832a25bd2c3a28
Mahdi <aparat1337@gmail.com>
2025-03-12 21:58:10 +0330

    add Program.cs

---
+++ Builder/Program.cs
@@ -0,0 +1,16 @@
+﻿using Builder.Concrete;
+using Builder.Model;
+
+SocialMediaPostBuilder builder = new SocialMediaPostBuilder();
+
+SocialMediaPost post = builder
+    .AddTitle("Book")
+    .AddContent("This book is perfect")
+    .AddAuthor("GOF")
+    .AddDatePosted(DateTime.Now)
+    .AddImageUri(new Uri("https://something.com"))
+    .AddLinks(new List<string>() { "a.com", "b.com", "c.com" })
+    .AddTags(new List<string>() { "Book", "DesignPattern", "System Design" })
+    .Build();
+
+Console.WriteLine(post.ToString());
