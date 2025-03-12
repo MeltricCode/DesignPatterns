ce1a0e9234e7635f96f0ac89a5696078fbe50340
Mahdi <aparat1337@gmail.com>
2025-03-12 21:55:07 +0330

    Add Builder DesignPattern

---
+++ Builder/Concrete/SocialMediaPostBuilder.cs
@@ -0,0 +1,65 @@
+﻿using Builder.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Builder.Concrete
+{
+    public class SocialMediaPostBuilder
+    {
+
+        private SocialMediaPost _post = new();
+
+
+        public SocialMediaPostBuilder AddTitle(string title)
+        {
+            _post.Title = title;
+            return this;
+        }
+
+        public SocialMediaPostBuilder AddContent(string content)
+        {
+            _post.Content = content;
+            return this;
+        }
+
+        public SocialMediaPostBuilder AddAuthor(string author)
+        {
+            _post.Author = author;
+            return this;
+        }
+
+        public SocialMediaPostBuilder AddDatePosted(DateTime datePosted)
+        {
+            _post.DatePosted = datePosted;
+            return this;
+        }
+
+        public SocialMediaPostBuilder AddTags(List<string> tags)
+        {
+            _post.Tags = tags;
+            return this;
+        }
+
+        public SocialMediaPostBuilder AddImageUri(Uri uri)
+        {
+            _post.ImageUri = uri;
+            return this;
+        }
+
+        public SocialMediaPostBuilder AddLinks(List<string> links)
+        {
+            _post.Links = links;
+            return this;
+        }
+
+        public SocialMediaPost Build()
+        {
+            return _post;
+        }
+
+
+    }
+}
---
+++ Builder/Model/SocialMediaPost.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Builder.Model
+{
+    public class SocialMediaPost
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string Author { get; set; }
+        public DateTime DatePosted { get; set; }
+        public List<string> Tags { get; set; }
+        public Uri ImageUri { get; set; }
+        public List<string> Links { get; set; }
+
+
+        public override string ToString()
+        {
+            var properties = typeof(SocialMediaPost).GetProperties();
+
+            string propertyNames = "";
+            foreach (var prop in properties)
+            {
+                if (prop.PropertyType.IsGenericType && prop.PropertyType.GetGenericTypeDefinition() == typeof(List<>))
+                {
+                    var values = prop.GetValue(this);
+
+                    if(values is List<string> listValue)
+                    {
+                        propertyNames += $"{prop.Name}: [{string.Join(", ", listValue)}], ";
+                    }
+                }
+                else
+                {
+                    propertyNames += $"{prop.Name}: {prop.GetValue(this)?.ToString()}, ";
+                }
+            }
+            propertyNames = propertyNames.TrimEnd(',',' ');
+
+            return propertyNames;
+        }
+    }
+}
