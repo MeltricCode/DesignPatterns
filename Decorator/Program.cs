4b468a87984b9c9f54919eab288bd2b67506e915
Mahdi <aparat1337@gmail.com>
2025-03-31 15:03:00 +0330

    Make Decorator More understandable

--- Decorator/Program.cs
+++ Decorator/Program.cs
@@ -7,11 +7,19 @@ extensibility while adhering to the Open/Closed Principle (OCP)
 by enabling dynamic behavior modifications without modifying existing code.
  */
 
-LevelBaseLogger logger = new(LogLevel.Info);
+// Base Logger
+Logger logger2 = new();
 
+logger2.Log("API has been initialized from Normal Logger");
+
+
+// Featured Logger
+LevelBaseLogger logger = new(LogLevel.Info);
 
 logger.Log(LogLevel.Info, "Acknowledgment sent to the provider.");
 
 logger.Log(LogLevel.Debug, "API has been initialized."); // This won't execute due to the minimum level set in LevelBaseLogger.
 
 logger.Log("API has been initialized."); // This will execute due to the implementation of the log method in the Logger class.
+
+
